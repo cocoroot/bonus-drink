403500684fab17f4a3a806bb0420cee6e50b5695
d820255 <yano-t16@mail.dnp.co.jp>
2016-03-09 20:20:16 +0900

    aa

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 #ruby "2.1.1"
 
-gem "rspec", "~> 2.14"
+gem "rspec"
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,8 @@
-require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
+require 'spec_helper'
+require './bonus_drink'
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(6)).to eq 8 }
+  it 'nomal end' do
+    expect(BonusDrink.total_count_for(6)).to eq 8
+  end
 end
